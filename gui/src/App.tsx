e5fd521459e5c47030a1e84a24f0e672c2822ef3
lehangajanayake <gajanayakelehan@gmail.com>
2026-09-25 15:42:32 +0930

    lading page changed back to telemtry

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -11,7 +11,7 @@ import { DEFAULT_CONFIG } from "./config";
 import backgroundImageFile from '@/assets/background-cropped.png';
 
 function App() {
-  const [page, setPage] = useState<PAGE>(PAGE.SETTINGS);
+  const [page, setPage] = useState<PAGE>(PAGE.TELEMETRY);
   const [portStatus, setPortStatus] = useState<STATUS>(STATUS.DISCONNECTED);
   const [transport, setTransport] = useState<"serial" | "websocket">(
     DEFAULT_CONFIG.connection.transport,
